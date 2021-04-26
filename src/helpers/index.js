e57538695903f06ea934cc105a6054ad2fca9470
sooditk <sooditkumarabc@gmail.com>
2021-04-26 19:31:07 +0530

    Changed index.js in Hooks, Helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { collatedTasks } from "../constants";
 
 export const collatedTasksExist = (selectedProject) => {
   collatedTasksExist.find((task) => task.key === selectedProject);
